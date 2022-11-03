ceab823236b980803ac406ea6123dd4734fa8479
schennamsetty-incomm <48800191+schennamsetty-incomm@users.noreply.github.com>
2022-11-03 11:46:50 -0400

    Property changes added

--- src/main/java/com/incomm/springbootsample/SpringBootTest.java
+++ src/main/java/com/incomm/springbootsample/SpringBootTest.java
@@ -3,6 +3,9 @@ package com.incomm.springbootsample;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,19 +14,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/services")
 public class SpringBootTest {
 	
+	private static Logger log = LogManager.getLogger(SpringBootTest.class);
+	
+	@Value("${spring.message1}")
+	private String message1;
+	
+	@Value("${spring.message2}")
+	private String message2;
+	
+	@Value("${spring.message3}")
+	private String message3;
+	
+	@Value("${spring.message4}")
+	private String message4;
+	
 	@GetMapping(value="/status")
     public String status(HttpServletRequest request,HttpServletResponse response) throws Exception {
-		return "SpringBootTest Service is up and running";
+		log.info("Inside status()");
+		return "SpringBootTest status() is up and running";
 	}
 	
 	@GetMapping(value="/ping")
     public String ping(HttpServletRequest request,HttpServletResponse response) throws Exception {
+		log.info("Inside ping()");
 		return "SpringBootTest ping() is up and running";
 	}
 	
 	@GetMapping(value="/check")
     public String check(HttpServletRequest request,HttpServletResponse response) throws Exception {
-		return "SpringBootTest check is up and running";
+		log.info("Inside check()");
+		return message1+" "+message2+" "+message3+" "+message4;
 	}
-
+	
 }
--- src/main/java/com/incomm/springbootsample/SpringbootSampleApplication.java
+++ src/main/java/com/incomm/springbootsample/SpringbootSampleApplication.java
@@ -2,12 +2,12 @@ package com.incomm.springbootsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource({"file:/opt/incomm-simplespringboot/config/testspringboot.properties" })
 @SpringBootApplication
 public class SpringbootSampleApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootSampleApplication.class, args);
 	}
-
 }
