4ff86890b1e6d2c29030571f38bc6b821a1a87ed
schennamsetty-incomm <48800191+schennamsetty-incomm@users.noreply.github.com>
2022-11-09 15:51:11 -0500

    changes to config

--- src/main/java/com/incomm/springbootsample/SpringBootTest.java
+++ src/main/java/com/incomm/springbootsample/SpringBootTest.java
@@ -6,12 +6,18 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/services")
+@Configuration
+@EnableCaching
+@RefreshScope
 public class SpringBootTest {
 	
 	private static Logger log = LogManager.getLogger(SpringBootTest.class);
@@ -28,8 +34,9 @@ public class SpringBootTest {
 	@Value("${spring.message4}")
 	private String message4;
 	
-	@Value("${fsapi.password}")
+	@Value("${fsapi.password}") 
 	private String message5;
+	 
 	
 	@GetMapping(value="/status")
     public String status(HttpServletRequest request,HttpServletResponse response) throws Exception {
